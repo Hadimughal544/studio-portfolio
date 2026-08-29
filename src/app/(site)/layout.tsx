b979887b8d72ddf899bdf7bb4f4902aa6ba1435f
Muhammad Haider <haider15012004@gmail.com>
2026-08-29 12:51:05 +0500

    reload error fixed

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,5 @@
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
-import { TransitionProvider } from "@/components/layout/TransitionProvider";
 import { Wattsapp } from "@/components/layout/Wattsapp";
 
 export default function SiteLayout({
@@ -11,9 +10,7 @@ export default function SiteLayout({
   return (
     <div className="flex min-h-screen flex-col overflow-x-hidden bg-surface text-foreground">
       <Header />
-      <main className="min-w-0 flex-1 pt-20">
-        <TransitionProvider>{children}</TransitionProvider>
-      </main>
+      <main className="min-w-0 flex-1 pt-20">{children}</main>
       <Footer />
       <Wattsapp />
     </div>
